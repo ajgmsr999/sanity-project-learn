6a25db430e42e8959b9ba4e0986794d98929d6a5
SIMAN <79779796+ajgmsr999@users.noreply.github.com>
2022-06-13 13:25:34 +0530

    sanity project create

---
+++ schemas/blog.js
@@ -0,0 +1,23 @@
+export default {
+    name: 'blog',
+    type: 'document',
+      title: 'Blog',
+    fields: [
+      {
+        name: 'title',
+        type: 'string',
+        title: 'Title'
+      },
+      {
+        name: 'content',
+        type: 'string',
+        title: 'content'
+      },
+      {
+        title: 'Launch Scheduled At',
+        name: 'launchAt',
+        type: 'datetime'
+      },
+    ]
+  }
+  
