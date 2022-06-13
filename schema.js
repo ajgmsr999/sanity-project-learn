fdf805b8b139b55215e02fe2b2aa28dd03eba4fd
SIMAN <79779796+ajgmsr999@users.noreply.github.com>
2022-06-13 16:39:43 +0530

    add jspage

--- schema.js
+++ schema.js
@@ -1,9 +1,10 @@
 // First, we must import the schema creator
 import createSchema from 'part:@sanity/base/schema-creator'
 import blog from './blog'
-
+import rich from './rich'
 // Then import schema types from any plugins that might expose them
 import schemaTypes from 'all:part:@sanity/base/schema-type'
+
 
 // Then we give our schema to the builder and provide the result to Sanity
 export default createSchema({
@@ -14,6 +15,8 @@ export default createSchema({
   types: schemaTypes.concat([
 
     /* Your types here! */
-    blog
+    blog,
+    rich
+    
   ]),
 })
